b69f33f259cd889653a74598482cc77ccdcd7761
niccolot <nico_tiezzi98@hotmail.it>
2023-04-28 13:43:52 +0200

    tokenization and prepro in separate .py

--- prepro.py
+++ prepro.py
@@ -1,174 +1,61 @@
 import tensorflow as tf
-from tensorflow_text.tools.wordpiece_vocab import bert_vocab_from_dataset as bert_vocab
-import tensorflow_text as tf_text
-import re
-import pathlib
-import numpy as np
+
+# hyper_parameters
+batch_size = 64
+max_tokens = 65
+buffer_size = 20000  # big number for the shuffling function
 
 
-def write_vocab_file(filepath, vocab):
-    with open(filepath, 'w') as f:
-        for token in vocab:
-            print(token, file=f)
-
-
-def build_vocabs_from_dataset(lang1_dataset,
-                              lang2_dataset,
-                              bert_vocab_args,
-                              save_vocabs=False,
-                              vocab1_path=None,
-                              vocab2_path=None):
+def teacher_forcing(lang1, lang2, tokenizer_lang1, tokenizer_lang2):
     """
-    given 2 tf Datasets objects with paired translated sentences returns the
-    bert vocabularies
+    returns the data  with the
+    'teacher forcing' training, so if one has to
+    translate lang1->lang2 ((lang1 input, lang2 input), shifted lang2)
+    i.e. the model's input are a sentence in lang1 for the encoder,
+    the same sentence in lang2 for the decoder (with causal padding) and
+    for the ground truth the sentence in lang2 but shifted of one token.
 
-    :param lang1_dataset: (tf.data.Dataset) first language dataset
-    :param lang2_dataset: (tf.data.Dataset) second language dataset
-    :param bert_vocab_args: (dict) arguments for the bert algorithm
-    :param save_vocabs: (bool) if one wants to save the vocabularies
-    :param vocab1_path: (str) path and name of the .txt file to be saved
-    :param vocab2_path: (str) path and name of the .txt file to be saved
+    In this way the model sees up to a certain token of the sentence in lang2
+    and has to predict the next, e.g. :
 
-    :return: vocabularies
-    """
+    lang1: 'i am writing things'
+    lang2: 'sto scrivendo cose'
 
-    vocab1 = bert_vocab.bert_vocab_from_dataset(lang1_dataset, **bert_vocab_args)
-    vocab2 = bert_vocab.bert_vocab_from_dataset(lang2_dataset, **bert_vocab_args)
+    'i am writing' -> encoder
+    'sto scrivendo' -> decoder
+    target = 'cose'
 
-    if save_vocabs:
-        write_vocab_file(vocab1_path, vocab1)
-        write_vocab_file(vocab2_path, vocab2)
-
-    return vocab1, vocab2
-
-
-def get_datasets(dataset):
-    """
-    given a .txt file with translations separated by '\t' returns 2 separate
-    tensorflow datasets of one language each
-
-    input: dataset (str): path to the dataset file
-    return:
+    :param lang1: (ragged tensor) batch of sentences in language 1
+    :param lang2: (ragged tensor) batch of sentences in language 2
+    :param tokenizer_lang1: tokenizer for language 1
+    :param tokenizer_lang2: tokenizer for language 2
+    :return: teacher forcing paired dataset
     """
 
-    dataset1, dataset2 = np.loadtxt(dataset,
-                                    usecols=(0, 1),
-                                    encoding='utf-8',
-                                    unpack=True,
-                                    dtype='str',
-                                    delimiter='\t')
+    lang1 = tokenizer_lang1(lang1)
+    lang1 = lang1[:, :max_tokens]  # trim longer sequences
+    lang1 = lang1.to_tensor()  # ragged to 0 padded dense tensor
 
-    dataset1 = tf.convert_to_tensor(dataset1)
-    dataset2 = tf.convert_to_tensor(dataset2)
+    lang2 = tokenizer_lang1(lang2)
 
-    dataset1 = tf.data.Dataset.from_tensor_slices(dataset1)
-    dataset2 = tf.data.Dataset.from_tensor_slices(dataset2)
+    # for input and labels the last and first token respectively will
+    # be dropped for the teacher forcing, so the +1
+    lang2 = lang2[:, :max_tokens+1]
+    lang2_input = lang2[:, :-1].to_tensor()  # drop last token
+    lang2_target = lang2[:, 1:].to_tensor()  # drop first token
 
-    return dataset1, dataset2
+    return (lang1, lang2_input), lang2_target
 
 
-class CustomTokenizer(tf.Module):
-    def __init__(self, reserved_tokens, vocab_path):
-        super().__init__()
-        self.tokenizer = tf_text.BertTokenizer(vocab_path, lower_case=True)
-        self._reserved_tokens = reserved_tokens
-        # makes the export work when the model needs an external file
-        self._vocab_path = tf.saved_model.Asset(vocab_path)
+def get_batches(dataset):
+    """
+    get the preprocessed dataset to be fed to the model in a teacher forcing fashion
 
-        vocab = pathlib.Path(vocab_path).read_text().splitlines()
-        self.vocab = tf.Variable(vocab)
-
-        # Create the signatures for export:
-
-        # Include a 'tokenize' signature for a batch of strings.
-        self.tokenize.get_concrete_function(
-            tf.TensorSpec(shape=[None], dtype=tf.string))
-
-        # Include `detokenize` and `lookup` signatures for:
-        #   * `Tensors` with shapes [tokens] and [batch, tokens]
-        #   * `RaggedTensors` with shape [batch, tokens]
-        self.detokenize.get_concrete_function(
-            tf.TensorSpec(shape=[None, None], dtype=tf.int64))
-        self.detokenize.get_concrete_function(
-            tf.RaggedTensorSpec(shape=[None, None], dtype=tf.int64))
-
-        self.lookup.get_concrete_function(
-            tf.TensorSpec(shape=[None, None], dtype=tf.int64))
-        self.lookup.get_concrete_function(
-            tf.RaggedTensorSpec(shape=[None, None], dtype=tf.int64))
-
-        # These `get_*` methods take no arguments
-        self.get_vocab_size.get_concrete_function()
-        self.get_vocab_path.get_concrete_function()
-        self.get_reserved_tokens.get_concrete_function()
-
-    def _add_start_end(self, ragged):
-        """
-        adds the 'start' and 'end' tokens at the beginning and
-        end of each sentence. This will come handy when the tokenized
-        text will be fed to the transformer
-
-        :param ragged: input dataset
-        :return: dataset with 'start' and 'end' tokens
-        """
-        start_tk = tf.argmax(tf.constant(self._reserved_tokens) == "[START]")
-        end_tk = tf.argmax(tf.constant(self._reserved_tokens) == "[END]")
-        count = ragged.bounding_shape()[0]
-        starts = tf.fill([count, 1], start_tk)
-        ends = tf.fill([count, 1], end_tk)
-
-        return tf.concat([starts, ragged, ends], axis=1)
-
-    def _cleanup_text(self, token_txt):
-        """
-        removes all but the 'unknown' tokens from the detokenized text
-        before turning it back into words.
-        It also joins the text with white
-        spaces, otherwise the output would be like [b'i', b'am'] and not
-        [b'i am']
-
-        :param token_txt: detokenized text to be tidied up
-        :return: readable detokenized text
-        """
-        # Drop the reserved tokens, except for "[UNK]".
-        bad_tokens = [re.escape(tok) for tok in self._reserved_tokens if tok != "[UNK]"]
-        bad_token_re = "|".join(bad_tokens)
-
-        bad_cells = tf.strings.regex_full_match(token_txt, bad_token_re)
-        result = tf.ragged.boolean_mask(token_txt, ~bad_cells)
-
-        # Join them into strings.
-        result = tf.strings.reduce_join(result, separator=' ', axis=-1)
-
-        return result
-
-    @tf.function
-    def tokenize(self, strings):
-        enc = self.tokenizer.tokenize(strings)
-        # Merge the `word` and `word-piece` axes.
-        enc = enc.merge_dims(-2, -1)
-        # adds the 'start' and 'end' tokens
-        enc = self._add_start_end(enc)
-        return enc
-
-    @tf.function
-    def detokenize(self, tokenized):
-        words = self.tokenizer.detokenize(tokenized)
-        # makes the detokenized text readable
-        return self._cleanup_text(words)
-
-    @tf.function
-    def lookup(self, token_ids):
-        return tf.gather(self.vocab, token_ids)
-
-    @tf.function
-    def get_vocab_size(self):
-        return tf.shape(self.vocab)[0]
-
-    @tf.function
-    def get_vocab_path(self):
-        return self._vocab_path
-
-    @tf.function
-    def get_reserved_tokens(self):
-        return tf.constant(self._reserved_tokens)
+    :param dataset: (lang1, lang2_input), lang2_target teacher forcing dataset
+    :return: preprocessed dataset
+    """
+    return dataset\
+        .shuffle(buffer_size)\
+        .batch(batch_size)\
+        .map(teacher_forcing, tf.data.AUTOTUNE)\
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
---
+++ tokenization.py
@@ -0,0 +1,171 @@
+import tensorflow as tf
+from tensorflow_text.tools.wordpiece_vocab import bert_vocab_from_dataset as bert_vocab
+import tensorflow_text as tf_text
+import re
+import pathlib
+import numpy as np
+
+
+def write_vocab_file(filepath, vocab):
+    with open(filepath, 'w') as f:
+        for token in vocab:
+            print(token, file=f)
+
+
+def build_vocabs_from_dataset(lang1_dataset,
+                              lang2_dataset,
+                              bert_vocab_args,
+                              save_vocabs=False,
+                              vocab1_path=None,
+                              vocab2_path=None):
+    """
+    given 2 tf Datasets objects with paired translated sentences returns the
+    bert vocabularies
+
+    :param lang1_dataset: (tf.data.Dataset) first language dataset
+    :param lang2_dataset: (tf.data.Dataset) second language dataset
+    :param bert_vocab_args: (dict) arguments for the bert algorithm
+    :param save_vocabs: (bool) if one wants to save the vocabularies
+    :param vocab1_path: (str) path and name of the .txt file to be saved
+    :param vocab2_path: (str) path and name of the .txt file to be saved
+
+    :return: vocabularies
+    """
+
+    vocab1 = bert_vocab.bert_vocab_from_dataset(lang1_dataset, **bert_vocab_args)
+    vocab2 = bert_vocab.bert_vocab_from_dataset(lang2_dataset, **bert_vocab_args)
+
+    if save_vocabs:
+        write_vocab_file(vocab1_path, vocab1)
+        write_vocab_file(vocab2_path, vocab2)
+
+    return vocab1, vocab2
+
+
+def get_datasets(dataset):
+    """
+    given a .txt file with translations separated by '\t' returns 2 separate
+    tensorflow datasets of one language each
+
+    input: dataset (str): path to the dataset file
+    return:
+    """
+
+    dataset1, dataset2 = np.loadtxt(dataset,
+                                    usecols=(0, 1),
+                                    encoding='utf-8',
+                                    unpack=True,
+                                    dtype='str',
+                                    delimiter='\t')
+
+    dataset1 = tf.convert_to_tensor(dataset1)
+    dataset2 = tf.convert_to_tensor(dataset2)
+
+    dataset1 = tf.data.Dataset.from_tensor_slices(dataset1)
+    dataset2 = tf.data.Dataset.from_tensor_slices(dataset2)
+
+    return dataset1, dataset2
+
+
+class CustomTokenizer(tf.Module):
+    def __init__(self, reserved_tokens, vocab_path):
+        super().__init__()
+        self.tokenizer = tf_text.BertTokenizer(vocab_path, lower_case=True)
+        self._reserved_tokens = reserved_tokens
+        # makes the export work when the model needs an external file
+        self._vocab_path = tf.saved_model.Asset(vocab_path)
+
+        vocab = pathlib.Path(vocab_path).read_text().splitlines()
+        self.vocab = tf.Variable(vocab)
+
+        # Create the signatures for tensorflow model save and export:
+        self.tokenize.get_concrete_function(
+            tf.TensorSpec(shape=[None], dtype=tf.string))
+        self.tokenize.get_concrete_function(
+            tf.RaggedTensorSpec(shape=[None], dtype=tf.string))
+
+        self.detokenize.get_concrete_function(
+            tf.TensorSpec(shape=[None, None], dtype=tf.int64))
+        self.detokenize.get_concrete_function(
+            tf.RaggedTensorSpec(shape=[None, None], dtype=tf.int64))
+
+        self.lookup.get_concrete_function(
+            tf.TensorSpec(shape=[None, None], dtype=tf.int64))
+        self.lookup.get_concrete_function(
+            tf.RaggedTensorSpec(shape=[None, None], dtype=tf.int64))
+
+        self.get_vocab_size.get_concrete_function()
+        self.get_vocab_path.get_concrete_function()
+        self.get_reserved_tokens.get_concrete_function()
+
+    def _add_start_end(self, ragged):
+        """
+        adds the 'start' and 'end' tokens at the beginning and
+        end of each sentence. This will come handy when the tokenized
+        text will be fed to the transformer
+
+        :param ragged: input dataset
+        :return: dataset with 'start' and 'end' tokens
+        """
+        start_tk = tf.argmax(tf.constant(self._reserved_tokens) == "[START]")
+        end_tk = tf.argmax(tf.constant(self._reserved_tokens) == "[END]")
+        count = ragged.bounding_shape()[0]
+        starts = tf.fill([count, 1], start_tk)
+        ends = tf.fill([count, 1], end_tk)
+
+        return tf.concat([starts, ragged, ends], axis=1)
+
+    def _cleanup_text(self, token_txt):
+        """
+        removes all but the 'unknown' tokens from the detokenized text
+        before turning it back into words.
+        It also joins the text with white
+        spaces, otherwise the output would be like [b'i', b'am'] and not
+        [b'i am']
+
+        :param token_txt: detokenized text to be tidied up
+        :return: readable detokenized text
+        """
+        # Drop the reserved tokens, except for "[UNK]".
+        bad_tokens = [re.escape(tok) for tok in self._reserved_tokens if tok != "[UNK]"]
+        bad_token_re = "|".join(bad_tokens)
+
+        bad_cells = tf.strings.regex_full_match(token_txt, bad_token_re)
+        result = tf.ragged.boolean_mask(token_txt, ~bad_cells)
+
+        # Join them into strings.
+        result = tf.strings.reduce_join(result, separator=' ', axis=-1)
+
+        return result
+
+    @tf.function
+    def tokenize(self, strings):
+        enc = self.tokenizer.tokenize(strings)
+        # Merge the `word` and `word-piece` axes.
+        enc = enc.merge_dims(-2, -1)
+        # adds the 'start' and 'end' tokens
+        enc = self._add_start_end(enc)
+        return enc
+
+    @tf.function
+    def detokenize(self, tokenized):
+        words = self.tokenizer.detokenize(tokenized)
+        # makes the detokenized text readable
+        return self._cleanup_text(words)
+
+    @tf.function
+    def lookup(self, token_ids):
+        # given the tokenized text returns the tokens as word-piece text
+        return tf.gather(self.vocab, token_ids)
+
+    @tf.function
+    def get_vocab_size(self):
+        return tf.shape(self.vocab)[0]
+
+    @tf.function
+    def get_vocab_path(self):
+        return self._vocab_path
+
+    @tf.function
+    def get_reserved_tokens(self):
+        return tf.constant(self._reserved_tokens)
